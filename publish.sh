e9fd8a1205225973af95eaf6ead69099e2a71f73
idorce <idorce@outlook.com>
2019-11-09 19:23:45 +0800

    Sperate upgrade script

--- publish.sh
+++ publish.sh
@@ -3,5 +3,3 @@ git push
 python3 -m pip install --user --upgrade setuptools wheel twine
 python3 setup.py sdist bdist_wheel
 python3 -m twine upload dist/*
-sleep 16
-pip install --upgrade torchtrain
