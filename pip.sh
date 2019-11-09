b99428fb24d02d6cab54996e187879cc0e37e5a7
idorce <idorce@outlook.com>
2019-11-09 17:40:08 +0800

    Install setuptools etc. before upload to pypi

--- pip.sh
+++ pip.sh
@@ -1,5 +1,6 @@
 rm -rf build dist torchtrain.egg-info
 git push
+python3 -m pip install --user --upgrade setuptools wheel twine
 python3 setup.py sdist bdist_wheel
 python3 -m twine upload dist/*
 sleep 6
