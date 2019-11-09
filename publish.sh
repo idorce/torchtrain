7cc319d554b49d78bbc21ae03028341713758929
idorce <idorce@outlook.com>
2019-11-09 22:28:09 +0800

    Add --verbose

--- publish.sh
+++ publish.sh
@@ -2,4 +2,4 @@ rm -rf build dist torchtrain.egg-info
 git push
 python3 -m pip install --user --upgrade setuptools wheel twine
 python3 setup.py sdist bdist_wheel
-python3 -m twine upload dist/*
+python3 -m twine upload dist/* --verbose
