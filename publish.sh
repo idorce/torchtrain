8caa751849436bee1bbd0656a6c8d10b20fea51d
idorce <idorce@outlook.com>
2019-11-11 13:09:52 +0800

    Modify upgrade part in publish script

--- publish.sh
+++ publish.sh
@@ -3,6 +3,7 @@ git push
 python3 -m pip install --user --upgrade setuptools wheel twine
 python3 setup.py sdist bdist_wheel
 python3 -m twine upload dist/* --verbose
-pip install --upgrade torchtrain
-pip install --upgrade torchtrain
-pip install --upgrade torchtrain
+pip install --upgrade torchtrain | grep -v satisfied
+pip install --upgrade torchtrain | grep -v satisfied
+pip install --upgrade torchtrain | grep -v satisfied
+pip search torchtrain
