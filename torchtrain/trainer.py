Bug fix: loss should average when grad accumulated

torchtrain/trainer.py
@@ -153,6 +153,12 @@ class Trainer:
             self.optimizer.step()
             self.optimizer.zero_grad()
 
+        def rescale_grad(scale):
+            for group in self.optimizer.param_groups:
+                for p in group["params"]:
+                    if p.grad is not None:
+                        p.grad /= scale
+
         def current_stats(reset=False, write=False):
             metrics = {}
             desc = f" epoch: {epoch:3d} "
@@ -166,24 +172,33 @@ class Trainer:
             data.set_description(desc)
             return metrics
 
-        def one_batch(batch):
+        is_train = phase == "train"
+        self.model.train(is_train)
+        data = tqdm(self.data_iter[phase], disable=not self.config["tqdm"])
+        self.optimizer.zero_grad()
+        batch_size_sum = 0
+        for batch in data:
             inputs, labels = self.batch_to_xy(batch, phase)
             with torch.set_grad_enabled(is_train):
                 outputs = self.model(inputs)
                 for criterion in self.criteria.values():
                     criterion.update(outputs, labels)
             if is_train:
-                self.criteria["loss"].get_batch_score().backward()
-                if (data.n + 1) % self.config["grad_accumulate_batch"] == 0:
-                    optim_step()
+                if self.config["grad_accumulate_batch"] < 1:
+                    self.criteria["loss"].get_batch_score().backward()
+                else:
+                    loss = self.criteria["loss"].get_batch_score()
+                    batch_size = labels.size(0)
+                    batch_size_sum += batch_size
+                    loss *= batch_size
+                    loss.backward()
+                    if (data.n + 1) % self.config[
+                        "grad_accumulate_batch"
+                    ] == 0:
+                        rescale_grad(batch_size_sum)
+                        optim_step()
+                        batch_size_sum = 0
             current_stats()
-
-        is_train = phase == "train"
-        self.model.train(is_train)
-        data = tqdm(self.data_iter[phase], disable=not self.config["tqdm"])
-        self.optimizer.zero_grad()
-        for batch in data:
-            one_batch(batch)
         optim_step()
         return current_stats(reset=True, write=True)
 