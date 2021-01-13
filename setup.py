902d90610bad42d60ae918ca3e68d93881e90411
idorce <idorce@outlook.com>
2021-01-13 19:22:26 +0800

    v0.4.12

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="torchtrain",
-    version="0.4.11",
+    version="0.4.12",
     author="HQ",
     author_email="idorce@outlook.com",
     description="A small tool for PyTorch training",
